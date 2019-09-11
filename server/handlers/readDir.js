ce646967a77aab77b9531152edf9cf5eee55be9c
supervueman <supervueman@gmail.com>
2019-09-11 10:14:46 +0400

    Optimize readDir method

--- server/handlers/readDir.js
+++ server/handlers/readDir.js
@@ -1,7 +1,5 @@
 const dirTree = require("directory-tree");
 
-module.exports = function readDir(dirPath) {
-  return dirTree(`../files/${dirPath}`, {
-    normalizePath: true
-  });
-};
+module.exports = dirPath => dirTree(`../files/${dirPath}`, {
+  normalizePath: true
+});
