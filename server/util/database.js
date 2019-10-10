b9f56b2c984090060f13ee6c05bf60ccfa5e3179
supervueman <supervueman@gmail.com>
2019-10-10 16:37:51 +0400

    Add mysql connection test

--- server/util/database.js
+++ server/util/database.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const operatorsAliases = require('../handlers/operatorsAliases');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: 'postgres',
+  dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   operatorsAliases,
   logging: false
