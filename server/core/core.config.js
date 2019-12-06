ff9d45950f9aa1c3865ec7d8b1cfbb8331a342e9
supervueman <supervueman@gmail.com>
2019-12-06 10:38:51 +0400

    Edits

--- server/core/core.config.js
+++ server/core/core.config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
   db_name: process.env.DB_NAME,
   db_user: process.env.DB_USER,
-  db_passsword: process.env.DB_PASSWORD,
+  db_password: process.env.DB_PASSWORD,
   db_dialect: process.env.DB_DIALECT,
   db_host: process.env.DB_HOST,
   cors_access_url: process.env.CORS_ACCESS_URL,
   header_authorization: `Basic ${Buffer.from(process.env.AUTHORIZATION_LOGIN + ':' + process.env.AUTHORIZATION_PASSWORD).toString('base64')}`,
   server_port: process.env.SERVER_PORT
-}
+};
--- server/core/db/index.js
+++ server/core/db/index.js
@@ -2,26 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const {
-	db_name,
-	db_user,
-	db_password,
-	db_dialect,
-	db_host
+  db_name,
+  db_user,
+  db_password,
+  db_dialect,
+  db_host
 } = require('../core.config');
-
-console.log('db_name', db_name);
-console.log('db_user', db_user);
-console.log('db_password', db_password);
-console.log('db_dialect', db_dialect);
-console.log('db_host', db_host);
 
 // Handlers
 const operatorsAliases = require('./operators_aliases');
 
 module.exports = new Sequelize(db_name, db_user, db_password, {
-	dialect: db_dialect,
-	host: db_host,
-	operatorsAliases,
-	// logging: true,
-	logging: (...msg) => console.log(msg)
+  dialect: db_dialect,
+  host: db_host,
+  operatorsAliases,
+  logging: false
 });
