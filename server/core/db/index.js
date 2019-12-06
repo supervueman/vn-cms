eead350374e85d96b00f8644c866103cebf092bf
supervueman <supervueman@gmail.com>
2019-12-06 10:34:33 +0400

    Test

--- server/core/db/index.js
+++ server/core/db/index.js
@@ -9,6 +9,12 @@ const {
 	db_host
 } = require('../core.config');
 
+console.log('db_name', db_name);
+console.log('db_user', db_user);
+console.log('db_password', db_password);
+console.log('db_dialect', db_dialect);
+console.log('db_host', db_host);
+
 // Handlers
 const operatorsAliases = require('./operators_aliases');
 
