a9b1d53925d6f8312f35a4aeead89d6dd9f878b0
supervueman <supervueman@gmail.com>
2019-12-06 10:28:44 +0400

    Test

--- server/core/db/index.js
+++ server/core/db/index.js
@@ -2,19 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const {
-  db_name,
-  db_user,
-  db_password,
-  db_dialect,
-  db_host
+	db_name,
+	db_user,
+	db_password,
+	db_dialect,
+	db_host
 } = require('../core.config');
 
 // Handlers
 const operatorsAliases = require('./operators_aliases');
 
 module.exports = new Sequelize(db_name, db_user, db_password, {
-  dialect: db_dialect,
-  host: db_host,
-  operatorsAliases,
-  logging: false
+	dialect: db_dialect,
+	host: db_host,
+	operatorsAliases,
+	logging: true
 });
