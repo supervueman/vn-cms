97dd2ac1dd64053d8364fd1654646f62c4cbdaa1
supervueman <supervueman@gmail.com>
2019-12-29 13:12:56 +0400

    Refacor resource type

--- server/components/resourcetype/controller/findAll.js
+++ server/components/resourcetype/controller/findAll.js
@@ -1,14 +1,19 @@
 const Model = require('../model');
 
 module.exports = async (req, res) => {
-  if (!req.rules.is_resources_read) {
+  if (!req.rules.is_resource_read) {
     res.status(403).send({
-      message: 'Access denied!'
+      message: 'Forbidden'
     });
     return;
   }
 
-  const items = await Model.findAll();
+  const items = await Model.findAll().catch(err => {
+    res.status(400).send({
+      message: 'Bad request'
+    });
+    return;
+  });
 
   res.status(200).send(items);
 };
