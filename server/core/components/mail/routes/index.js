13e966edf3ea541dbf4a9c653910520b579df373
supervueman <supervueman@gmail.com>
2020-03-10 19:52:22 +0400

    Add mail to swagger

--- server/core/components/mail/routes/index.js
+++ server/core/components/mail/routes/index.js
@@ -7,6 +7,42 @@ const controller = require('../controller');
 // Middleware
 const profileByApiKey = require('../../../../middleware/profileByApiKey');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Mail
+ *   description: Mail management
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /mail:
+ *    post:
+ *      summary: Send email
+ *      tags: [Mail]
+ *      parameters:
+ *        - in: header
+ *          name: x-api-key
+ *          required: true
+ *          schema:
+ *            type: string
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *      responses:
+ *        "200":
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message: string
+ *      security:
+ *        - basicAuth: []
+ */
 router.post('/', profileByApiKey, controller.send);
 
 module.exports = router;
