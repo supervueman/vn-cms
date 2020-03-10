f4e279558856b5dae3f7a636ddda79ecc73292e2
supervueman <supervueman@gmail.com>
2020-03-10 19:46:26 +0400

    Add authenticate to swagger

--- server/components/authenticate/routes/index.js
+++ server/components/authenticate/routes/index.js
@@ -4,8 +4,73 @@ const controller = require('../controller');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Authenticate
+ *   description: Authenticate management
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /authenticate/login-by-email:
+ *    post:
+ *      summary: Login by email
+ *      tags: [Authenticate]
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *      responses:
+ *        "200":
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  id: string
+ *                  access_token: string
+ *      security:
+ *        - basicAuth: []
+ */
 router.post('/login-by-email', controller.loginByEmail);
 
+/**
+ * @swagger
+ * path:
+ *  /authenticate/login-by-phone:
+ *    post:
+ *      summary: Login by phone
+ *      tags: [Authenticate]
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *      responses:
+ *        "200":
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  id: string
+ *                  access_token: string
+ *      security:
+ *        - basicAuth: []
+ */
 router.post('/login-by-phone', controller.loginByPhone);
 
 module.exports = router;
