e4d1e517240e4bd90667b4f656aac857da2c79f5
supervueman <supervueman@gmail.com>
2020-04-30 08:48:45 +0400

    Add logger to resourcetypr

--- server/components/resourcetype/controller/findAll.js
+++ server/components/resourcetype/controller/findAll.js
@@ -2,13 +2,15 @@ const Model = require('../model');
 
 module.exports = async (req, res) => {
   if (!req.rules.is_resource_read) {
+    logger('error', 'resourcetype', 403, 'findAll.js');
     res.status(403).send({
       message: 'Forbidden'
     });
     return;
   }
 
-  const items = await Model.findAll().catch(err => {
+  const items = await Model.findAll().catch((err) => {
+    logger('error', 'resourcetype', 400, 'findAll.js', err);
     res.status(400).send({
       message: 'Bad request'
     });
--- server/components/resourcetype/controller/findOne.js
+++ server/components/resourcetype/controller/findOne.js
@@ -2,15 +2,17 @@ const Model = require('../model');
 
 module.exports = async (req, res) => {
   if (!req.rules.is_resource_type_read) {
+    logger('error', 'resourcetype', 403, 'findOne.js');
     res.status(403).send({
       message: 'Forbidden'
     });
     return;
   }
 
-  const filter = JSON.parse(req.query.filter || "{}");
+  const filter = JSON.parse(req.query.filter || '{}');
 
-  const items = await Model.findOne(filter).catch(err => {
+  const items = await Model.findOne(filter).catch((err) => {
+    logger('error', 'resourcetype', 403, 'findOne.js', err);
     res.status(400).send({
       message: 'Bad request'
     });
