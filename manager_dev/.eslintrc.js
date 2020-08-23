2f4901c69069ea17daece855c4c53ec6fe69e4d9
supervueman <supervueman@gmail.com>
2020-08-23 15:27:43 +0400

    Edit eslint

--- manager_dev/.eslintrc.js
+++ manager_dev/.eslintrc.js
@@ -3,21 +3,9 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 2018,
-    sourceType: 'module',
-    'vue/max-attributes-per-line': [
-      'error',
-      {
-        singleline: 1,
-        multiline: {
-          max: 1,
-          allowFirstLine: false
-        }
-      }
-    ]
+    sourceType: 'module'
   },
   rules: {
     'vue/html-quotes': ['error', 'double' | 'single']
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
   }
 };
