8844effa980cda97bf48c8948ce36594b6b6ad10
supervueman <supervueman@gmail.com>
2021-03-06 22:26:32 +0400

    Replace indexOf to includes

--- server/core/cors/index.js
+++ server/core/cors/index.js
@@ -1,10 +1,9 @@
 const { cors_access_url, header_authorization } = require('../core.config');
 
 module.exports = (req, res, next) => {
-  const allowedOrigins = cors_access_url.split(', ');
   const origin = req.headers.origin;
 
-  if (allowedOrigins.indexOf(origin) > -1) {
+  if (cors_access_url.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   } else {
     return res.sendStatus(403);
@@ -26,12 +25,12 @@ module.exports = (req, res, next) => {
     return res.sendStatus(200);
   }
 
-  if (req.url.indexOf('files') <= 0) {
+  if (req.url.includes('files')) {
     if (req.headers.authorization === header_authorization) {
       next();
     } else {
       logger('error', 'cors', 401, 'index.js', 'Authorization is not valid');
-
+      
       sendRes({ res, status: 401 });
     }
   } else {
